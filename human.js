780e64ae23529e43f3ca8d0b6296fa7e62d99352
Seolhee Kim <seolhee@upspringmedia.com>
2017-06-14 20:21:21 -1000

    create human.js and passes tests

---
+++ human.js
@@ -0,0 +1,21 @@
+class Human {
+  constructor(name){
+    this.name = name
+  }
+
+  sayName(){
+    return this.name
+  }
+}
+
+// class Person {
+
+// }
+
+module.exports = Human //node ...
+// module.exports = Person //node ...
+
+// module.exports = {
+//   Human,
+//   Person
+// } // if i want to reveal everything
